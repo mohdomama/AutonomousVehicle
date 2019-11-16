d6e6f6fa7991a7a6bce64193ef4728574fab9455
Mohd Omama <mohd.omama@gmail.com>
2019-11-16 10:53:07 +0530

    feat: Object Detection and Following

--- Motion/Movement.py
+++ Motion/Movement.py
@@ -1,10 +1,12 @@
 import RPi.GPIO as gpio
 import time
+import camera
+import numpy as np
 
 class Movement:
     def __init__ (self):
         gpio.setmode(gpio.BOARD)
-        gpio.setwarnings(False)
+        gpio.setwarnings(True)
         gpio.setup(7 , gpio.OUT)
         gpio.setup(11 , gpio.OUT)
         gpio.setup(13 , gpio.OUT)
@@ -23,7 +25,7 @@ class Movement:
         self.p4.start(0)
         self.p3.start(0)
 
-    def forward(self, cycle):
+    def backward(self, cycle):
         self.p1.ChangeDutyCycle(cycle)
         self.p2.ChangeDutyCycle(0)
 
@@ -31,7 +33,7 @@ class Movement:
         self.p4.ChangeDutyCycle(0)
        
     
-    def backward(self, cycle):
+    def forward(self, cycle):
         self.p1.ChangeDutyCycle(0)
         self.p2.ChangeDutyCycle(cycle)
 
@@ -54,14 +56,56 @@ class Movement:
         self.p3.ChangeDutyCycle(cycle)
         self.p4.ChangeDutyCycle(0)
         
+    def stop(self):
+        self.p1.ChangeDutyCycle(0)
+        self.p2.ChangeDutyCycle(0)
+
+        self.p3.ChangeDutyCycle(0)
+        self.p4.ChangeDutyCycle(0)
+        
 
 
     def move(self):
-        print('Here')
-        self.backward(100)
-        print('Blah')
-        while True:
-            pass
+        i=0
+        lower = [
+        np.array([115, 0, 0]),  # Red
+        np.array([87, 0, 0]),   # Yellow
+        np.array([50, 0, 0])]   # Green
+        upper = [
+        np.array([135, 255, 255]),
+        np.array([92, 255, 255]),
+        np.array([77, 255, 255])]
+        print('Love')
+        cam = camera.Camera()
+        
+        try:
+            while True:
+                image = cam.read()
+                cam.mask_image(lower[i], upper[i])
+                centroid = cam.centroid_if_object_present()
+                if centroid:
+                    cx, cy = centroid
+                    center = float(image.shape[1] / 2)
+                    
+                    diff_per = (cx - center) / (2 * center)
+                    
+                    print('Diff_Per ', diff_per)
+                    if diff_per > 0.2:
+                        print('Going Right')
+                        self.right(100)
+                    elif diff_per < -0.2:
+                        print('Going Left')
+                        self.left(100)
+                    else:
+                        self.forward(100)
+                else:
+                    self.stop()
+                    
+                    
+        except Exception as e:
+            print(e)
+            cam.tearDown()
+         
        
 
     def cleanup(self):
@@ -76,9 +120,11 @@ class Movement:
 if __name__ == "__main__":
     movement = Movement()
     try:
-        movement.move()
-        movement.cleanup()
-    except:
+        movement.forward(50)
+        time.sleep(2)
+        
+    except Exception as e:
+        print(e)
         movement.cleanup()
 
 
